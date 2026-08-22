4d5723617a7119753732a68fdc5d1d52703b9e84
IramKhan99517 <163287065+IramKhan99517@users.noreply.github.com>
2026-08-22 21:18:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
